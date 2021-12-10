87931da216a2cecf427223a5c1f70ed0152f637a
FlutLab.io <>
2021-12-10 14:32:17 +0000

    Autosave

--- lib/data.dart
+++ lib/data.dart
@@ -10,5 +10,17 @@ List<Map<String, dynamic>> studentData = [
     "name": "Mis Falguny",
     'phone': 120235,
     'roll': 12
+  },
+  {
+    'id': 1,
+    "name": "Mohiuddin Tarek",
+    'phone': 120235,
+    'roll': 12
+  },
+  {
+    'id': 2,
+    "name": "Mis Falguny",
+    'phone': 120235,
+    'roll': 12
   }
 ];
